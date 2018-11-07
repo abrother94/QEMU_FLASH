6eef9638d5fb4550687a06c08ad214c2a7de5603
nick_huang <nick_huang@accton.com>
2018-11-07 00:46:24 +0000

    Add rootfs with device node

--- Build.sh
+++ Build.sh
@@ -2,24 +2,34 @@
 
 #Build Qemu 2.6
 
-tar zxvf rootfs.tar.gz
+SUDOCHK=`sudo -v`
+
+if [ "${SUDOCHK}" != "" ];then
+    echo "You need have sudo right to build this...Sorry!"
+fi
+
+echo "extract sample rootf"
+sudo tar zxvf rootfs.tar.gz
 
 cd qemu
+echo "Build QEMU...."
 ./Build.sh "2.6.0" 
 cd ../
 
 #Build uboot
-
+echo "Build u-boot...."
 cd u-boot-armdev.git/
 ./Build.sh 
 cd ../
 
 #Build kernel
 cd kernel
+echo "Build kernel...."
 ./Build.sh "4.2.1" 
 
 cd ../
 
 #Build busybox
+echo "Build busybox...."
 cd busybox
 ./Build.sh
