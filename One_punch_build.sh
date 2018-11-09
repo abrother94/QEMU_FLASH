5fe1f4da11e7294dd9087caf2d47038b6ffd0391
abrother94 <abrother94@gmail.com>
2018-11-09 16:45:12 +0800

    Update One_punch_build.sh

--- One_punch_build.sh
+++ One_punch_build.sh
@@ -6,8 +6,10 @@ if [ "${QEMU_CONTAINER}" == "" ];then
     PWD=`pwd`
     docker run --privileged  -i -t -e DOCKER_IMAGE=opennetworklinux/builder8:1.9 --name QEMU-One-Punch -d -v /lib/modules:/lib/modules -v ${PWD}:${PWD} -e USER=${USERNAME} --net host -w ${PWD} -e HOME=/home/${USERNAME} -v /home/${USERNAME}:/home/${USERNAME} opennetworklinux/builder8:1.9 /bin/docker_shell --user ${USERNAME}:1000 -c bash
     docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${USERNAME} ./"
+    docker attach QEMU-One-Punch
 else
     docker start QEMU-One-Punch 
     docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${USERNAME} ./"
+    docker attach QEMU-One-Punch    
 fi
 
