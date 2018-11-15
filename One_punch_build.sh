8264daf77cf3e6b79d7f800edb2bc38dc1943090
abrother94 <abrother94@gmail.com>
2018-11-15 08:56:50 +0800

    Update One_punch_build.sh

--- One_punch_build.sh
+++ One_punch_build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 USERNAME=`whoami`
+uid=`id -u`
+gid=`id -g`
+
 QEMU_CONTAINER=`docker ps -a | grep QEMU-One-Punch | awk '{print $1}'`
 if [ "${QEMU_CONTAINER}" == "" ];then
     # do not have docker image, pull it.
     PWD=`pwd`
     docker run --privileged  -i -t -e DOCKER_IMAGE=opennetworklinux/builder8:1.9 --name QEMU-One-Punch -d -v /lib/modules:/lib/modules -v ${PWD}:${PWD} -e USER=${USERNAME} --net host -w ${PWD} -e HOME=/home/${USERNAME} -v /home/${USERNAME}:/home/${USERNAME} opennetworklinux/builder8:1.9 /bin/docker_shell --user ${USERNAME}:1000 -c bash
-    docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${USERNAME} ./"
+    docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${gid} ./"
+    `sudo chown -R ${USERNAME}:${gid} ./`    
     docker attach QEMU-One-Punch
 else
     docker start QEMU-One-Punch 
-    docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${USERNAME} ./"
+    docker exec QEMU-One-Punch /bin/docker_shell -c "./Build.sh;sudo chown -R ${USERNAME}:${gid} ./"
+    `sudo chown -R ${USERNAME}:${gid} ./`      
     docker attach QEMU-One-Punch    
 fi
 
