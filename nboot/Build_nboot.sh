8f03d1104ddd52128fe62fd377681a5495dd957e
abrother94 <abrother94@gmail.com>
2018-11-27 13:21:31 +0800

    Update Build_nboot.sh

--- nboot/Build_nboot.sh
+++ nboot/Build_nboot.sh
@@ -4,7 +4,7 @@
 
 
 if [ "$1" == "C" ];then
-    rm *.o ; rm nboot.map ; rm nboot.elf.srec ;rm nboot.bin ;rm nboot.elf; rm ../nboot.bin 
+    rm *.o ; rm nboot.map ; rm nboot.elf.srec ;rm nboot.bin ;rm nboot.elf; rm ../nboot.bin ; rm ../nboot.elf
 else
 
 arm-linux-gnueabi-gcc  -D__ASSEMBLY__ -g  -Os   -fno-strict-aliasing  -fno-common -ffixed-r8 -msoft-float  -D__KERNEL__ -DTEXT_BASE=0x01000000  -fno-builtin -ffreestanding -nostdinc -isystem /usr/lib/gcc/arm-linux-gnueabi/4.9/include -pipe  -DCONFIG_ARM -D__ARM__ -march=armv5te -mabi=apcs-gnu -mno-thumb-interwork -c -o start.o start.S
@@ -17,5 +17,5 @@ arm-linux-gnueabi-objcopy -O srec nboot.elf nboot.elf.srec
 
 arm-linux-gnueabi-objcopy --gap-fill=0xff -O binary nboot.elf nboot.bin
 
-cp nboot.bin ../
+cp nboot.bin ../ ;cp nboot.elf ../
 fi
